a7ada0ef5e382e1f3d28d8eee38a0afcf397c3d8
KaungHtetMon29 <kaunghtetmon456@gmail.com>
2023-09-23 03:18:51 +0630

    err fix

--- src/layout/bloglayout.tsx
+++ src/layout/bloglayout.tsx
@@ -1,10 +1,3 @@
-import nft from "../assets/nft.png";
-import savbank from "../assets/savacc.png";
-import bank from "../assets/bank.png";
-import av1 from "../assets/av1.png";
-import av2 from "../assets/av2.png";
-import av3 from "../assets/av3.png";
-
 import Bloghero from "../components/blog/hero";
 import Question from "../components/blog/Q&A";
 import { useParams } from "react-router-dom";
@@ -12,68 +5,6 @@ import { useParams } from "react-router-dom";
 import Related from "../components/blog/Related";
 import Latestnew from "../components/blog/latesnew";
 function BlogLayout() {
-  const Malertarray = [
-    {
-      title: "Major Crypto Exchanges Face Action Over",
-      image: nft,
-      acc: {
-        name: "Darlene Robertson",
-        date: "5/27/15",
-        post: "Japan’s financial watchdog is reportedly planning to force improvements at a number of licensed cryptocurrency...",
-        avatar: av1,
-      },
-    },
-    {
-      title: "FinCEN issues regulations requiring that financial",
-      image: savbank,
-      acc: {
-        name: "Courtney Henry",
-        date: "22 Dec 2021",
-        post: "FinCEN is issuing final rules under the Bank Secrecy Act to clarify and strengthen customer due diligence...",
-        avatar: av2,
-      },
-    },
-    {
-      title: "US State Bank Supervisors Issue Model",
-      image: bank,
-      acc: {
-        name: "Jerome Bell",
-        date: "22 Dec 2021",
-        post: "The Conference of State Bank Supervisors (CSBS) has released a draft proposal for regulating digital currency...",
-        avatar: av3,
-      },
-    },
-    {
-      title: "Major Crypto Exchanges Face Action Over",
-      image: nft,
-      acc: {
-        name: "Darlene Robertson",
-        date: "5/27/15",
-        post: "Japan’s financial watchdog is reportedly planning to force improvements at a number of licensed cryptocurrency...",
-        avatar: av1,
-      },
-    },
-    {
-      title: "FinCEN issues regulations requiring that financial",
-      image: savbank,
-      acc: {
-        name: "Courtney Henry",
-        date: "22 Dec 2021",
-        post: "FinCEN is issuing final rules under the Bank Secrecy Act to clarify and strengthen customer due diligence...",
-        avatar: av2,
-      },
-    },
-    {
-      title: "US State Bank Supervisors Issue Model",
-      image: bank,
-      acc: {
-        name: "Jerome Bell",
-        date: "22 Dec 2021",
-        post: "The Conference of State Bank Supervisors (CSBS) has released a draft proposal for regulating digital currency...",
-        avatar: av3,
-      },
-    },
-  ];
   const { id } = useParams();
   return (
     <>
