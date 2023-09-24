fe0754610c80b21726c5f84b1a070bace663c085
KaungHtetMon29 <kaunghtetmon456@gmail.com>
2023-09-25 03:11:04 +0630

    finale

--- src/components/moneyalertcard.tsx
+++ src/components/moneyalertcard.tsx
@@ -4,7 +4,7 @@ import { Arrow, Clock } from "../assets/svgs";
 function Malert({ posts, index }: { posts: any; index: number }) {
   const navi = useNavigate();
   return (
-    <div className=" bg-white rounded-lg flex flex-col items-start 2xl:w-96 xl:w-80 lg:w-64 w-[80%] lg:gap-6 gap-2   shadow-lg ">
+    <div className="h-[36rem] bg-white rounded-lg flex flex-col items-start 2xl:w-96 xl:w-80 lg:w-64 w-[80%] lg:gap-6 gap-2   shadow-lg ">
       {posts !== undefined && (
         <>
           <img
@@ -12,12 +12,12 @@ function Malert({ posts, index }: { posts: any; index: number }) {
             alt={posts.title}
             className={`object-cover object-center w-full xl:h-56 h-40  rounded-t-lg `}
           />
-          <div className="px-5 pt-0 pb-8 flex flex-col gap-4 text-left w-full">
+          <div className="px-5 pt-0 pb-8 flex flex-col gap-4 text-left h-full w-full">
             <div className="xl:text-xl lg:text-lg font-semibold line-clamp-1">
               {posts.title}
             </div>
-            <div className="w-full text-left">
-              <div className="flex flex-col xl:gap-8 gap-4">
+            <div className="w-full text-left h-full flex flex-col">
+              <div className="flex flex-col xl:gap-8 gap-4 flex-1">
                 <div className="flex xl:items-center lg:items-start sm:flex-row xl:gap-0 gap-2 flex-col text-base text-center ">
                   <div className="flex items-center gap-2 grow">
                     <img
@@ -35,23 +35,25 @@ function Malert({ posts, index }: { posts: any; index: number }) {
                     <p>{posts.date}</p>
                   </div>
                 </div>
-                <div className="xl:text-base lg:text-sm">
-                  <p className="xl:line-clamp-6 lg:line-clamp-3">
-                    {posts.post} ...
+                <div className="xl:text-base lg:text-sm flex flex-1">
+                  <p className="xl:line-clamp-6 lg:line-clamp-3 ">
+                    {posts.post}
                   </p>
                 </div>
-                <p
-                  className="flex gap-3 items-center font-bold cursor-pointer"
-                  onClick={() => {
-                    console.log(posts.postid);
-                    navi(`/blog/${posts.postid}`);
-                  }}
-                >
-                  Read more
-                  <div className="w-6">
-                    <Arrow color="black" />
-                  </div>
-                </p>
+                <div className="flex">
+                  <p
+                    className="flex gap-3 items-center font-bold cursor-pointer"
+                    onClick={() => {
+                      console.log(posts.postid);
+                      navi(`/blog/${posts.postid}`);
+                    }}
+                  >
+                    Read more
+                    <div className="w-6">
+                      <Arrow color="black" />
+                    </div>
+                  </p>
+                </div>
               </div>
             </div>
           </div>
